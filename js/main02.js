e9834ae627f5ad896fac40366b49c69523a8435e
Takafumi Kojima <tkfmkjm@gmail.com>
2018-08-07 16:41:09 -0700

    Major update
    Major update

--- js/main02.js
+++ js/main02.js
@@ -1,23 +1,42 @@
-// Koko (Aha! experience) main javascript
+/*
 
-// Useragent
+Koko Main JavaScript
+by Takafumi Kojima
+jQuery version:
+
+*/
+
+/***************************
+Deactivate right click and drag
+****************************/
 $(function () {
-    var ua = navigator.userAgent;
-    if (ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0) {
-        // Smartphone
-        console.log("smartphone");
-    } else if (ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0) {
-        // Tablet
-        console.log("tablet");
-    } else {
-        // PC
-        console.log("pc");
-        $("#start_button_text").text("Click to Start");
-    }
+  $('img').attr('onmousedown', 'return false');
+  $('img').attr('onselectstart', 'return false');
+  $('img').attr('oncontextmenu', 'return false');
+});
+
+/***************************
+User-agent
+****************************/
+$(function () {
+  var ua = navigator.userAgent;
+  if (ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0) {
+    // Smartphone
+    console.log("Device: Mobile");
+  } else if (ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0) {
+    // Tablet
+    console.log("Device: Tablet");
+  } else {
+    // PC: Change 'tap' to 'click'
+    console.log("Device: PC");
+    $("#start_button_text").text("Click to Start");
+  }
 })
 
-// Load thumbnails, Get image URLs automatically
-$(function() {
+/***************************
+Load thumbnails and get image paths automatically
+****************************/
+$(function () {
 
   if (document.getElementById("toppage") != null) {
     console.log("index.html");
@@ -38,24 +57,68 @@ $(function() {
 
 });
 
-// Fadein at load
+// Fadein at load (to avoid a player seeing sudden change of images)
 $("body").css({
   opacity: '0'
 });
-setTimeout(function() {
+setTimeout(function () {
   $("body").stop().animate({
     opacity: '1'
   }, 1500);
 }, 1500);
 
-// Hide URL bar on mobile device
-window.onload = function() {
+
+/***************************
+Core function
+****************************/
+function getPixelDataOfImg(img, event) {
+  let canvas = document.createElement('canvas');
+  canvas.width = img.width;
+  canvas.height = img.height;
+  canvas.getContext('2d').drawImage(img, 0, 0, img.width, img.height);
+  return canvas.getContext('2d').getImageData(event.offsetX, event.offsetY, 1, 1).data[3];
+}
+
+/***************************
+Run at load
+****************************/
+window.onload = function () {
+
+  console.log('C:', countCorrect, 'W:', countWrong, 'ON:', startFinish);
+
+  // Detecting alpha
+  document.querySelector('#answer_area').addEventListener('click', function (event) {
+
+    let PixelData = getPixelDataOfImg(document.querySelector('#answer_area'), event);
+
+    if (PixelData == 255 && startFinish == 1) {
+      console.log("Right area clicked");
+      $("body").css("background-color", "#48C9B0");
+      $('#instruction').text("Correct! Tap the picture to retry or pick other picture!");
+      $('#before').addClass('blinking');
+      $('#progress').stop();
+    } else if (PixelData == 0 && startFinish == 1) {
+      console.log("Wrong area clicked");
+      $("body").css("background-color", "#EC7063"); // Turn background red when wrong
+      $("#frame").addClass('shake');
+      $('#instruction').text("Wrong! Tap the picture to retry.");
+
+    } else {
+      console.log("Need Start Game");
+    }
+
+    console.log("PixelData(alpha):", PixelData);
+  })
+
+  // Hide URL bar on mobile device
   setTimeout(scrollTo, 100, 0, 1);
 }
 
-// Loading screen
-$(window).on('load', function() {
-  $(function() {
+/***************************
+Loading Screen transition
+****************************/
+$(window).on('load', function () {
+  $(function () {
     //setTimeout(function(){
     $("#loading").fadeOut();
     $("#main")
@@ -65,20 +128,14 @@ $(window).on('load', function() {
       .animate({
         opacity: "1"
       }, 2000);
-    //},3000);
   });
 });
 
-// No right click or drag
-$(function() {
-  $('img').attr('onmousedown', 'return false');
-  $('img').attr('onselectstart', 'return false');
-  $('img').attr('oncontextmenu', 'return false');
-});
-
-// Pointer
-$(function() {
-  $('#frame').click(function(e) {
+/***************************
+Pointer
+****************************/
+$(function () {
+  $('#frame').click(function (e) {
     if (countWrong == 0 && startFinish == 1) {
       var areaOffset = $('#frame').offset();
       var offsetTop = ((e.pageY) - (areaOffset.top));
@@ -93,87 +150,96 @@ $(function() {
   });
 });
 
-//Progress bar
-$(function() {
+/***************************
+After Start-Button Clicked
+****************************/
 
-  $('#start_button').click(function() {
+// Setting Changing Time
+changeTime = 10000; //milli second
 
-    $('#progress').animate({
-      width: '0',
-    }, {
-      duration: 10000,
-      easing: 'linear',
-    }); //end of animate
-  });
-});
-
-//Start button, Fade effect time
-$(function() {
+$(function () {
   startFinish = 0;
-  $('#start_button').click(function() {
+  $('#start_button').click(function () {
     if (startFinish == 0) {
+
+      //Start-Button Dissapear
       $('#start_button').animate({
         opacity: '0',
       }, {
         duration: 200,
         easing: 'linear',
       }); //end of #start_button animate
-      $('#start_button').css('display', 'none');
 
+      //Progress Bar
+      $('#progress').animate({
+        width: '0',
+      }, {
+        duration: changeTime,
+        easing: 'linear',
+      });
+
+      //Image Changing Time
+      $('#before').animate({
+        opacity: '0',
+      }, {
+        duration: changeTime,
+        easing: 'linear',
+      });
+
+      $('#start_button').css('display', 'none');
       $("#before").removeClass("darkImg");
       $("#start_button").data("playingOnOff", startFinish = 1);
       $("#frame").data("hitWrong", countWrong = 0);
       $("#frame").data("hitCorrect", countCorrect = 0);
+
       console.log('C:', countCorrect, 'W:', countWrong, 'ON:', startFinish);
-      $('#before').animate({
-        opacity: '0',
-      }, {
-        duration: 10000,
-        easing: 'linear',
-      }); //end of animate
+
     } //end of if statement
     return false; //for the if statement
   }); // end of click function
-}); //End of start button
+});
 
-//Activate and deactivate retry button (Not used in this version)
-$(function() {
-  setInterval(function() {
-    if ($('img#before').css('opacity') == 0) {
-      $('#retry').css('color', 'black');
-    } else {
-      $('#retry').css('color', '#D5D8DC');
-    }
-  }, 100);
-}); //end of activate deactivate retry button
+/***************************
+Activate and deactivate retry button ([!] Not used in this version)
+****************************/
+// $(function () {
+//   setInterval(function () {
+//     if ($('img#before').css('opacity') == 0) {
+//       $('#retry').css('color', 'black');
+//     } else {
+//       $('#retry').css('color', '#D5D8DC');
+//     }
+//   }, 100);
+// });
 
-
-//Instruction massages
-$(function() {
-  setInterval(function() {
-
+/***************************
+Instruction Massages
+****************************/
+$(function () {
+  setInterval(function () {
     if ($('img#before').css('opacity') == 0 && countWrong <= 0) {
-      $('#instruction').text("The change is done. Tap a part of picture.");
+      $('#instruction').text("The change is over. Tap a part of picture.");
     } else if (countWrong == 0 && countCorrect == 0 && startFinish == 0) {
       $('#instruction').text("Find a part of the picture that changes gradually.");
     }
-
   }, 100);
-}); //end of instruction messages
+});
 
-//Social Media share links
-$(function() {
+/***************************
+Social Media Share Links
+****************************/
+$(function () {
   var url = document.URL;
   var encodedurl = encodeURIComponent(document.URL);
   var title = document.title;
   var encodedtitle = encodeURIComponent(document.title);
 
-  //twitter
+  //Twitter
   var twitter = jQuery(".socialTw").attr("href");
   twitter = "https://twitter.com/intent/tweet?url=" + encodedurl + "&text=" + encodedtitle;
   jQuery("a.socialTw").attr("href", twitter);
 
-  //facebook
+  //Facebook
   var facebook = jQuery("a.socialFb").attr("href");
   facebook = "https://www.facebook.com/sharer/sharer.php?u=" + url;
   jQuery("a.socialFb").attr("href", facebook);
@@ -189,13 +255,13 @@ $(function() {
   jQuery("a.socialGp").attr("href", googlePlus);
 });
 
-
-//Retry button
-$(function() {
-  $("#frame").on("click", function() {
+/***************************
+Retry Button
+****************************/
+$(function () {
+  $("#frame").on("click", function () {
 
     if (countWrong == 1 && startFinish == 1) {
-
       $('#progress').css('width', '100%');
       $('#progress').stop();
       $('#before').stop();
@@ -237,7 +303,7 @@ $(function() {
       console.log('C:', countCorrect, 'W:', countWrong, 'ON:', startFinish);
       $("body").css("background-color", "");
 
-      // hide the pointer
+      // Hide the pointer
       $('#locatedpoint').css("display", "none");
 
       $('#start_button').animate({
@@ -248,82 +314,122 @@ $(function() {
       }); //end of #start_button animate
       $('#start_button').css('display', 'flex');
 
-
     } //if opacity is 0
   }); // retry click
-}); // end
+}); // end of retry button
 
-
-//Alphapicker Setting
-$(function() {
+/***************************
+Score
+****************************/
+//Wrong Area Selected
+$(function () {
   countWrong = 0;
   countCorrect = 0;
-  $("#frame").click(function() {
+  $("#frame").click(function () {
+    // If wrong area is clicked after game started add 1 to countWong
     if (startFinish == 1) {
       $("#frame").data("hitWrong", countWrong = 1);
       console.log('C:', countCorrect, 'W:', countWrong, 'ON:', startFinish);
-      if (countCorrect == 0 && countWrong >= 1 && startFinish == 1) {
-        console.log('C:', countCorrect, 'W:', countWrong, 'ON:', startFinish);
-        $("body").css("background-color", "#EC7063");
-        $("#frame").addClass('shake');
-        $('#instruction').text("Wrong! Tap the picture to retry.");
 
-      }
-      return false;
+      // // And if wrong area is clicked
+      // if (countCorrect == 0 && countWrong >= 1 && startFinish == 1) {
+      //   console.log('C:', countCorrect, 'W:', countWrong, 'ON:', startFinish);
+      // }
+      // return false;
+
     } //end of if statement
     return false; //for the if statement
-
   }); // end of click function
 
-  window.addEventListener("load", function() {
-    var a = document.getElementById("kotae"), // get a-element
-      img = document.getElementById("answer_area"), // get img-element
-      url = a.href, // get a.herf
-      alpha;
-
-    try {
-      /* Give the img-element to Alphapicker & get Alphapicker object*/
-      alpha = AlphaPicker(img);
-    } catch (e) {
-      /* Error message */
-      console.log(e.name + ":" + e.message);
-      return;
-    }
-
-    /* deactivate a-element event*/
-    a.className = "alphapicker";
-    a.addEventListener("click", function(e) {
-      e.preventDefault();
-    }, false);
-
-    /* Alphapickerオブジェクトのメソッドでマウスオーバーとリンクを設定します */
-    alpha.mousemove(function(e) {
-      /* イベントオブジェクトのalphaプロパティに、マウス座標のアルファ値が入っています */
-      if (e.alpha) {
-        this.className = "hover";
-      } else {
-        this.className = "";
-      }
-    }).mouseout(function(e) {
-      this.className = "";
-    }).click(function(e) {
-      if (e.alpha) {
-
-        // What to do after clicking the currect area
+  //Right Area Selected
+  $(function () {
+    $("#frame").click(function () {
+      // If right area is clicked after game started add 1 to countWong
+      if (startFinish == 1) {
         $("#frame").data("hitCorrect", countCorrect = 1);
+        console.log('C:', countCorrect, 'W:', countWrong, 'ON:', startFinish);
 
-        if (countCorrect == 1 && countWrong == 0 && startFinish == 1) {
-          console.log('C:', countCorrect, 'W:', countWrong, 'ON:', startFinish);
-          $("body").css("background-color", "#48C9B0");
-          $('#instruction').text("Correct! Tap the picture to retry or pick other picture!");
-          $('#before').addClass('blinking');
-          $('#progress').stop();
-        } //end of if C1,W0,ON1
+        // // And if right area is clicked
+        // if (countCorrect == 1 && countWrong == 0 && startFinish == 1) {
+        //   console.log('C:', countCorrect, 'W:', countWrong, 'ON:', startFinish);
+        // }
+        // return false;
 
-        return false;
-        // /What to do after clicking the currect area
+      } //end of if statement
+      return false; //for the if statement
+    }); // end of click function
+  });
 
-      } //end of if (e.alpha)
-    });
-  }, false);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // window.addEventListener("load", function () {
+  //   var a = document.getElementById("kotae"), // get a-element 'kotae(means answer)'
+  //     img = document.getElementById("answer_area"), // get img-element
+  //     url = a.href, // get a.herf
+  //     alpha;
+
+  //   try {
+  //     /* Give the img-element to Alphapicker & get Alphapicker object*/
+  //     alpha = AlphaPicker(img);
+  //   } catch (e) {
+  //     /* Error message */
+  //     console.log(e.name + ":" + e.message);
+  //     return;
+  //   }
+
+  //   /* deactivate a-element event*/
+  //   a.className = "alphapicker";
+  //   a.addEventListener("click", function (e) {
+  //     e.preventDefault();
+  //   }, false);
+
+  //   /* Alphapickerオブジェクトのメソッドでマウスオーバーとリンクを設定します (Setting link) */
+  //   alpha.mousemove(function (e) {
+  //     /* イベントオブジェクトのalphaプロパティに、マウス座標のアルファ値が入っています. (The event object's 'alpha' propaty containcs the coodinate of alpha area.) */
+  //     if (e.alpha) {
+  //       this.className = "hover";
+  //     } else {
+  //       this.className = "";
+  //     }
+  //   }).mouseout(function (e) {
+  //     this.className = "";
+  //   }).click(function (e) {
+  //     if (e.alpha) {
+
+  //       // What to do after clicking the currect area
+  //       $("#frame").data("hitCorrect", countCorrect = 1);
+
+  //       if (countCorrect == 1 && countWrong == 0 && startFinish == 1) {
+  //         console.log('C:', countCorrect, 'W:', countWrong, 'ON:', startFinish);
+  //         // $("body").css("background-color", "#48C9B0");
+  //         // $('#instruction').text("Correct! Tap the picture to retry or pick other picture!");
+  //         // $('#before').addClass('blinking');
+  //         // $('#progress').stop();
+  //       } //end of if C1,W0,ON1
+
+  //       return false;
+  //       // /What to do after clicking the currect area
+
+  //     } //end of if (e.alpha)
+  //   });
+  // }, false);
+
+
 });
--- play/blueredbikini/index.php
+++ play/blueredbikini/index.php
@@ -3,14 +3,15 @@
 
 <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#">
 
-<meta charset="utf-8">
+	<meta charset="utf-8">
 
-<meta property="og:title" content="KOKO - Find a Difference!">
-<meta property="og:type" content="website" />
-<meta property="og:description" content="Find a part of picture that changes gradually! Enjoy the best a-ha experience game!" />
-<?php include($_SERVER['DOCUMENT_ROOT'] . '/php/ogp.php'); //Insert OGP URLs ?>
-<meta property="fb:app_id" content="268596930288915" />
-<meta name="twitter:card" content="summary_large_image" />
+	<meta property="og:title" content="KOKO - Find a Difference!">
+	<meta property="og:type" content="website" />
+	<meta property="og:description" content="Find a part of picture that changes gradually! Enjoy the best a-ha experience game!"
+	/>
+	<?php include($_SERVER['DOCUMENT_ROOT'] . '/php/ogp.php'); //Insert OGP URLs ?>
+	<meta property="fb:app_id" content="268596930288915" />
+	<meta name="twitter:card" content="summary_large_image" />
 
 	<!-- No zoom on mobile -->
 	<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
@@ -18,11 +19,9 @@
 	<!-- jQuery -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
-<!-- jQuery UI -->
-	<script
-	  src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"
-	  integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU="
-	  crossorigin="anonymous"></script>
+	<!-- jQuery UI -->
+	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU="
+	    crossorigin="anonymous"></script>
 
 	<!-- General JavaScript/jQuery -->
 	<script src="/js/main02.js"></script>
@@ -41,16 +40,23 @@
 
 	<title>Koko Web Vesion: Beta</title>
 
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+	<script>
+		(function (i, s, o, g, r, a, m) {
+			i['GoogleAnalyticsObject'] = r;
+			i[r] = i[r] || function () {
+				(i[r].q = i[r].q || []).push(arguments)
+			}, i[r].l = 1 * new Date();
+			a = s.createElement(o),
+				m = s.getElementsByTagName(o)[0];
+			a.async = 1;
+			a.src = g;
+			m.parentNode.insertBefore(a, m)
+		})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
-  ga('create', 'UA-103050142-1', 'auto');
-  ga('send', 'pageview');
-
-</script></head>
+		ga('create', 'UA-103050142-1', 'auto');
+		ga('send', 'pageview');
+	</script>
+</head>
 
 <body>
 	<p id="main_navigation"></p>
@@ -68,44 +74,51 @@
 		<div id="frame">
 
 			<!-- Pointer -->
-			<span id="locatedpoint"><img src="/images/arrow.png" alt=""></span>
+			<span id="locatedpoint">
+				<img src="/images/arrow.png" alt="">
+			</span>
 
 			<!-- Start button -->
 			<div id="start_button">
 				<p id="start_button_text">Tap to Start</p>
 			</div>
 
-<?php include($_SERVER['DOCUMENT_ROOT'] . '/php/image_loader.php'); //Image Loader ?>
+			<!-- load answer, before, after image -->
+			<?php include($_SERVER['DOCUMENT_ROOT'] . '/php/image_loader.php'); //Image Loader ?>
 
-<div id="progress"></div>
-	</div>	<!-- /#frame -->
-		</div>		<!-- /#main loading-->
+			<div id="progress"></div>
+		</div>
+		<!-- /#frame -->
+	</div>
+	<!-- /#main loading-->
 
 	<!-- Navigation -->
 	<p id="navigation">
-		<i class="fa fa-home" aria-hidden="true"></i><a href="/">Home</a> |
-		Share This Picture on
-		 <a href="#" target="_blank" title="Share on Twitter" class="socialTw"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
-		 <a href="#" target="_blank" title="Share on Facebook" class="socialFb"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
+		<i class="fa fa-home" aria-hidden="true"></i>
+		<a href="/">Home</a> | Share This Picture on
+		<a href="#" target="_blank" title="Share on Twitter" class="socialTw">
+			<i class="fa fa-twitter-square" aria-hidden="true"></i>
+		</a>
+		<a href="#" target="_blank" title="Share on Facebook" class="socialFb">
+			<i class="fa fa-facebook-square" aria-hidden="true"></i>
+		</a>
 	</p>
 
-<div id="grid"></div>
+	<div id="grid"></div>
 
-<div id="google_ad">
-	<p class="ads_claim">AD<p/>
-	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-	<!-- Koko - large mobile banner -->
-	<ins class="adsbygoogle"
-	     style="display:inline-block;width:320px;height:100px"
-	     data-ad-client="ca-pub-4042220637899031"
-	     data-ad-slot="9818199756"></ins>
-	<script>
-	(adsbygoogle = window.adsbygoogle || []).push({});
-	</script>
+	<div id="google_ad">
+		<p class="ads_claim">AD
+			<p/>
+			<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+			<!-- Koko - large mobile banner -->
+			<ins class="adsbygoogle" style="display:inline-block;width:320px;height:100px" data-ad-client="ca-pub-4042220637899031" data-ad-slot="9818199756"></ins>
+			<script>
+				(adsbygoogle = window.adsbygoogle || []).push({});
+			</script>
 
-</div>
+	</div>
 
-<div id="credit">&copy; Takafumi Kojima</div>
+	<div id="credit">&copy; Takafumi Kojima</div>
 
 </body>
 
